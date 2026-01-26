18f236de8d86ea710c35b8bfbb21619d429db126
arsen <arsenijpopov865@gmail.com>
2026-01-26 12:29:31 +0200

    https://kompege.ru/variant?kim=25146073

--- Homeworks/Homework-27-01-26/24-10724.py
+++ Homeworks/Homework-27-01-26/24-10724.py
@@ -2,6 +2,6 @@ from re import *
 with open(r'../Homework-27-01-26/24_10724.txt') as file:
     data = file.readline()
 
-pattern = '[^0|G-Z}]+'
+pattern = '[^0|G-Z]+'
 matches = [match.group() for match in finditer(pattern, data)]
 print(len(max(matches, key=len)))
