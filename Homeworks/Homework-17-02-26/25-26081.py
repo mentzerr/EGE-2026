4f9e1e0ad7d46212765ee32dfd9ead045edf3509
arsen <arsenijpopov865@gmail.com>
2026-02-17 16:14:56 +0200

    Разобрали задание с домашки

--- Homeworks/Homework-17-02-26/25-26081.py
+++ Homeworks/Homework-17-02-26/25-26081.py
@@ -1,6 +1,17 @@
-for N in range(111_111, 1_000_000):
-    for NUM in range(1, 100):
-        if '0' not in str(N) and N == 3 ** (NUM) + N:
-            if N % 113 == 0 and N % 2 == 1:
-                print(N)
+def f(num):
+    for i1 in range(113, num, 226):
+        for i2 in range(0, 13): # степень двойки, что < 1_000_000
+            if i1 + 3 ** i2 == num:
+                return i2
+    return 0
+
+
+k = 0
+for N in range(111_112, 1_000_000, 2): # N = нечет + нечет = чет
+    if S := f(N):
+        if '0' not in str(N):
+            print(N, S)
+            k += 1
+            if k == 5:
                 break
+
