53e658e16073e639a5f6d91eba4975a1c0df386a
arsen <arsenijpopov865@gmail.com>
2026-02-24 17:45:55 +0200

    Доделали дз

--- Homeworks/Homework-24-02-26/23-2341.py
+++ Homeworks/Homework-24-02-26/23-2341.py
@@ -1,8 +1,9 @@
-def f(start, k):
-    if start in range(1000, 1025):
-        if k == 8: return {start}
+def f(start, k=0):
+    if k == 8:
+        if start in range(1000, 1025): return {start}
+        return set()
     return f(start + 1, k + 1) | f(start + 5, k + 1) | f(start * 3, k + 1)
 
-print(len(f(1, 0)))
+print(len(f(1)))
 
 
