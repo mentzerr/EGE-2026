06798b8b9cf29cc5b032d2228cbd3a660c0c0444
arsen <arsenijpopov865@gmail.com>
2026-03-06 16:14:51 +0200

    Улучшили решение

--- Homeworks/Task-17/17-23905.py
+++ Homeworks/Task-17/17-23905.py
@@ -4,14 +4,6 @@ def check1(n, m):
 def check2(n):
     return str(n)[-1] == str(n)[-2]
 
-def check3(list):
-    summary = 0
-    for i in list:
-        for j in i:
-            if str(j)[-1] == str(j)[-2]:
-                summary += j
-
-    return summary
 
 a = [int(x) for x in open('17_23905.txt')]
 
@@ -22,5 +14,5 @@ ans = []
 for x, y, w, z in zip(a, a[1:], a[2:], a[3:]):
     if check1(x, max_el) + check1(y, max_el) + check1(w, max_el) + check1(z, max_el) == 2:
         if check2(x) + check2(y) + check2(z) + check2(w) == 1:
-            ans.append([x, y, w, z])
-print(len(ans), check3(ans))
+            ans.append(*[i for i in [x,y,w,z] if check2(i)])
+print(len(ans), sum(ans))
