e326ca76ff23957bf285812deba42b6af8643867
arsen <arsenijpopov865@gmail.com>
2026-03-11 17:55:18 +0200

    Сменил название у папки

--- Homeworks/Task-17/17-27301.py
+++ Homeworks/Task-17/17-27301.py
@@ -1,13 +1,12 @@
-
-
 a = [int(x) for x in open('17_27301.txt')]
 
-
-max_el = max([int(x) for x in a if str(abs(x))[:2] == '45'])
+max_el = max([x for x in a if str(abs(x))[:2] == '45'])
 
 ans = []
 for x, y, z in zip(a, a[1:], a[2:]):
-    if (x < 0) + (y < 0) + (z < 0) == 1 and x + y + z >= max_el:
-            if abs(x + y + z) % 100 == 45:
-                ans.append(abs(x + y + z))
-print(len(ans))
+    if ((x < 0) + (y < 0) + (z < 0) == 1) and x + y + z >= max_el:
+        if abs(x + y + z) % 100 == 45:
+            ans.append(x + y + z)
+print(len(ans), min(ans))
+
+
