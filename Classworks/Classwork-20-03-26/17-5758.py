861c257d65ee38e6ecf592992ad39a154513be72
arsen <arsenijpopov865@gmail.com>
2026-03-27 18:03:49 +0200

    Потом надо решить

--- Classworks/Classwork-20-03-26/17-5758.py
+++ Classworks/Classwork-20-03-26/17-5758.py
@@ -1,13 +1,13 @@
-from itertools import combinations
-
-with open('17_5758.txt') as file:
-    data = [int(x) for x in file]
-
-for first, second in combinations(data, 2):
-
-
-########################################################
-
-for first in range(len(data) - 1):
-    for second in range(first + 1, len(data)):
-
+# from itertools import combinations
+#
+# with open('17_5758.txt') as file:
+#     data = [int(x) for x in file]
+#
+# for first, second in combinations(data, 2):
+#
+#
+# ########################################################
+#
+# for first in range(len(data) - 1):
+#     for second in range(first + 1, len(data)):
+#
